1f4155279bdf26ce829252c16f71569e3b0c1a1b
Denis627 <disacom627@gmail.com>
2018-06-13 11:13:38 +0300

    Customized admin page

--- cv_to_pdf/admin.py
+++ cv_to_pdf/admin.py
@@ -6,18 +6,76 @@ from cv_to_pdf.models import *
 
 class GroupInline(admin.TabularInline):
     model = Group
+    extra = 1
 
 
 class DepartmentAdmin(admin.ModelAdmin):
     inlines = [
-        GroupInline
+        GroupInline,
+    ]
+
+
+class TechnicalExpertiseInline(admin.TabularInline):
+    model = TechnicalExpertise
+    extra = 1
+
+
+class ToolAndFrameworkInline(admin.TabularInline):
+    model = ToolAndFramework
+    extra = 1
+
+
+class ProjectInline(admin.StackedInline):
+    model = Project
+    extra = 1
+
+
+class CommunicationInline(admin.TabularInline):
+    model = Communication
+    extra = 1
+
+
+class EducationInline(admin.TabularInline):
+    model = Education
+    extra = 1
+
+
+class ResumeAdmin(admin.ModelAdmin):
+    inlines = [
+        TechnicalExpertiseInline,
+        ToolAndFrameworkInline,
+        ProjectInline,
+        CommunicationInline,
+        EducationInline,
+    ]
+
+
+class PersonInline(admin.TabularInline):
+    model = Person
+    extra = 1
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [
+        PersonInline,
+    ]
+
+
+class ResumeInline(admin.TabularInline):
+    model = Resume
+    extra = 1
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        ResumeInline,
     ]
 
 
 admin.site.register(Department, DepartmentAdmin)
-admin.site.register(Group)
-admin.site.register(Person)
-admin.site.register(Resume)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Resume, ResumeAdmin)
 admin.site.register(TechnicalExpertise)
 admin.site.register(ToolAndFramework)
 admin.site.register(Project)
